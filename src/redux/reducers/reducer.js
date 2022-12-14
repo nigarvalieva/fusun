ec5d60d7782c22ec44b41341c6673f611d6f5148
RaminMcdli <raminmacidli@gmail.com>
2022-12-13 23:30:28 -0800

    first v

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,25 +1,28 @@
 const initialState = {
-  favorites: [],
-  movies: []
+  title: "",
+  listID: "",
+  favs: [],
+  movies: [],
+  movieDetails: [],
+  listMovies: [],
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case "ADD_TO_FAVORITES":
-      const movie = state.movies.find((item) => item.id === action.payload.id);
-      let favorites = [...state.favorites, movie];
-
-      return {
-        ...state,
-        favorites,
-      };
+      const newState = { ...state };
+            const id = action.payload.id;
+            const match = newState.movies.find((item) => item.imdbID === id);
+            if (match) {
+                newState.favs = [...newState.favs, { ...match }];
+            }
+            return newState;
     case "DELETE_FROM_FAVORITES":
     case "FIND_MOVIE":
-      const movies = [...action.payload.movies]
-        return {
-          ...state,
-          movies
-        };
+      return {
+        ...state,
+        movies: action.payload.movies,
+    };
     default:
       return state;
   }
